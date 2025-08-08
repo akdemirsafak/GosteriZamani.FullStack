597ff6f0869a171cea4c3d521426212f4c51c405
Şafak Akdemir <akdemirsafak34@hotmail.com>
2025-08-08 23:43:32 +0300

    Audit Interceptor eklendi.

---
+++ GosteriZamani.API/Interceptors/AuditInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using GosteriZamani.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace GosteriZamani.API.Interceptors;
+
+public class AuditInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        var context = eventData.Context;
+        if (context is null)
+        {
+            return new ValueTask<InterceptionResult<int>>(result);
+        }
+        var entries = context.ChangeTracker.Entries<IAuditableEntity>();
+        foreach (var entry in entries)
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    entry.Entity.CreatedBy = "System"; // Replace with actual user identifier
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                    entry.Entity.UpdatedBy = "System"; // Replace with actual user identifier
+                    break;
+                case EntityState.Deleted:
+                    entry.State = EntityState.Modified; // Change to Modified to set DeletedAt and DeletedBy
+                    entry.Entity.DeletedAt = DateTime.UtcNow;
+                    entry.Entity.DeletedBy = "System"; // Replace with actual user identifier
+                    break;
+            }
+        }
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
--- GosteriZamani.API/Program.cs
+++ GosteriZamani.API/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using GosteriZamani.API.DbContexts;
 using GosteriZamani.API.Filters;
+using GosteriZamani.API.Interceptors;
 using GosteriZamani.API.Models.Category;
 using GosteriZamani.API.Services;
 using GosteriZamani.API.Validations.Category;
@@ -24,22 +25,22 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<GosteriZamaniDbContext>(options =>
+
+builder.Services.AddSingleton<AuditInterceptor>();
+
+builder.Services.AddDbContext<GosteriZamaniDbContext>((sp,options) =>
 {
+    var interceptor = sp.GetService<AuditInterceptor>()!;
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         options =>
         {
             options.MigrationsAssembly(Assembly.GetAssembly(typeof(GosteriZamaniDbContext))!.GetName().Name);
-        });
+        }).AddInterceptors(interceptor);
 });
-
-
 
 //Register services
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-
 
 var app = builder.Build();
 
